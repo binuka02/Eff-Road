ec68b8768fa1d453100a2304e289f938a458fb27
Binuka Silva <wabgsilva@gmail.com>
2023-05-15 08:55:13 +0530

    Voice output when detect a road sign

--- components/Features.js
+++ components/Features.js
@@ -1,30 +1,27 @@
-import { StyleSheet, Text, Alert, View, Dimensions, Button,TouchableOpacity,Image } from 'react-native'
-import React from 'react'
-import MapView, {Marker} from 'react-native-maps';
-import { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, Alert, View, Dimensions, Button, TouchableOpacity, Image } from 'react-native'
+import React, { useEffect, useState, useRef } from 'react'
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import axios from 'axios'
-import { API_URL } from '@env'; 
+import { API_URL } from '@env';
 import tw from 'tailwind-react-native-classnames';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useSelector } from 'react-redux';
 import { featureLocationsData } from '../slices/navSlice';
 import { Camera, CameraType } from 'expo-camera';
+import * as Speech from 'expo-speech';
 
 const Features = () => {
+  // Model Calling
 
-    //Model Calling
-
-    const [type, setType] = useState(CameraType.back);
+  const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const cameraRef = useRef(null);
-//   const [capturedImage, setCapturedImage] = useState(null);
-
+  const [spokenClassName, setSpokenClassName] = useState('');
 
   useEffect(() => {
     console.log("Camera Opened")
-    // parsedLocation = JSON.parse(currentLocation)
     async function getPermission() {
       const { granted } = await Camera.requestCameraPermissionsAsync();
       if (granted) {
@@ -32,16 +29,12 @@ const Features = () => {
       }
     }
     getPermission();
-    
   }, []);
-
 
   const handleCapture = async () => {
     if (permission) {
       try {
         const image = await captureImage();
-                // setCapturedImage(image.uri);
-
         await sendImageToBackend(image.uri);
       } catch (error) {
         Alert.alert('Error', 'Failed to capture or send the image.');
@@ -59,7 +52,6 @@ const Features = () => {
     return null;
   };
 
-
   const sendImageToBackend = async (imageUri) => {
     try {
       const formData = new FormData();
@@ -69,7 +61,6 @@ const Features = () => {
         name: 'image.jpg',
       });
 
-      // const response = await axios.post('http://192.168.1.3:5000/detect-road-sign',formData);
       const response = await fetch(
         'http://192.168.1.3:5000/detect-road-sign',
         {
@@ -81,16 +72,23 @@ const Features = () => {
         }
       )
 
-      const {class_name} = await response.json();
+      const { class_name } = await response.json();
 
-      console.log(class_name)
+      console.log(class_name);
 
-      // Use text-to-speech library (e.g., react-native-tts) to convert class_name to voice output
+      setSpokenClassName(class_name);
     } catch (error) {
       console.log(error);
       Alert.alert('Error', 'Failed to communicate with the backend.');
     }
   };
+
+  useEffect(() => {
+    if (spokenClassName) {
+      Speech.speak(spokenClassName);
+    }
+  },)
+
 
 
 
@@ -357,7 +355,7 @@ await axios.post(API_URL+"/location",{
             <TouchableOpacity
             onPress={handleCapture}
             >
-                <View style={tw`bg-red-400 pl-10 pr-10`}>
+                <View style={tw`bg-white pl-10 pr-10`}>
                     <Text></Text>
                 </View>
             </TouchableOpacity>
